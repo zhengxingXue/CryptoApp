d69cbb2fc7d9e7904c822eac487067574881ca00
zhengxingXue <zx252cornell@gmail.com>
2021-08-25 19:02:30 -0400

    #12: Creating a new Model and View to display market statistics

---
+++ Crypto/Core/Home/Views/HomeStatsView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeStatsView.swift
+//  Crypto
+//
+//  Created by Jim's MacBook Pro on 8/25/21.
+//
+
+import SwiftUI
+
+struct HomeStatsView: View {
+    
+    @EnvironmentObject private var vm: HomeViewModel
+    
+    @Binding var showPortfolio: Bool
+    
+    var body: some View {
+        HStack {
+            ForEach(vm.statistic) { stat in
+                StaticticView(stat: stat)
+                    .frame(width: UIScreen.main.bounds.width / 3)
+            }
+        }
+        .frame(width: UIScreen.main.bounds.width, alignment: showPortfolio ? .trailing : .leading)
+    }
+}
+
+struct HomeStatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeStatsView(showPortfolio: .constant(false))
+            .environmentObject(dev.homeVM)
+    }
+}
